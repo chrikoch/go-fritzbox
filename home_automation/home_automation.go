4017448d163e566416b1bef4f40003a3eff7a3b4
Christian Koch <chri.koch@gmail.com>
2016-12-09 21:51:24 +0100

    first home automation tests

---
+++ home_automation/home_automation.go
@@ -0,0 +1,49 @@
+package home_automation
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"gitlab.com/chri.koch/fritzbox_util/config"
+)
+
+type HomeAutomation struct {
+	Config config.Config
+}
+
+func (h *HomeAutomation) SwitchList(sessionId string) {
+	req, err := http.NewRequest("GET", h.Config.HomeAutomationUrl(), nil)
+	if err != nil {
+		log.Println(err)
+		return //InvalidSessionId, err
+	}
+
+	query := req.URL.Query()
+
+	query.Add("switchcmd", "getswitchlist")
+	query.Add("sid", sessionId)
+
+	req.URL.RawQuery = query.Encode()
+
+	var client http.Client
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return //InvalidSessionId, err
+	}
+
+	defer resp.Body.Close()
+
+	log.Printf("Got HTTP result %v.", resp.StatusCode)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return //InvalidSessionId, err
+	}
+
+	log.Println(string(body))
+
+}
