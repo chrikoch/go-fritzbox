07c99760f1ea3735f1d2ef904f5b4ba9f4a98ba1
Christian Koch <chri.koch@gmail.com>
2016-12-11 20:58:27 +0100

    removed reading the config file

--- config/config.go
+++ config/config.go
@@ -1,11 +1,6 @@
 //holds the configuration
 package config
 
-import (
-	"encoding/json"
-	"io/ioutil"
-)
-
 type Config struct {
 	BoxURL   string
 	Password string
@@ -17,24 +12,4 @@ func (c *Config) LoginUrl() string {
 
 func (c *Config) HomeAutomationUrl() string {
 	return "http://" + c.BoxURL + "/webservices/homeautoswitch.lua"
-}
-
-//Read a config in JSON format from filname and return as Config struct
-func New(filename string) (Config, error) {
-	var c Config
-	file, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return c, err
-	}
-
-	err = json.Unmarshal(file, &c)
-	if err != nil {
-		return c, err
-	}
-
-	if len(c.BoxURL) == 0 {
-		c.BoxURL = "fritz.box"
-	}
-
-	return c, nil
 }
