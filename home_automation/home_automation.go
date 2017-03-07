78c2aa7504cc0afe3d2a1543a53d244ce4fd1442
Christian Koch <chri.koch@gmail.com>
2017-03-07 21:09:01 +0100

    added power consumption

--- home_automation/home_automation.go
+++ home_automation/home_automation.go
@@ -4,12 +4,49 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/chrikoch/go-fritzbox/config"
 )
 
 type HomeAutomation struct {
 	Config config.Config
+}
+
+//Returns current powerconsumption auf AIN as mW
+func (h *HomeAutomation) CurrentPowerConsumption(sessionId, ain string) (power int, err error) {
+	req, err := http.NewRequest("GET", h.Config.HomeAutomationUrl(), nil)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	query := req.URL.Query()
+
+	query.Add("switchcmd", "getswitchpower")
+	query.Add("sid", sessionId)
+	query.Add("ain", ain)
+
+	req.URL.RawQuery = query.Encode()
+
+	var client http.Client
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	return strconv.Atoi(strings.TrimSpace(string(body)))
 }
 
 func (h *HomeAutomation) SwitchList(sessionId string) {
